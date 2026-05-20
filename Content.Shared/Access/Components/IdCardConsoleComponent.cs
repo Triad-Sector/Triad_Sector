2ee4f8f4a07a9ea6afbc5f2e12d3bafd089c6a16
TheRealMasterChief117 <officialtrollfacexxx@gmail.com>
2026-05-20 16:20:26 -0400

    fixes and ID card console

--- Content.Shared/Access/Components/IdCardConsoleComponent.cs
+++ Content.Shared/Access/Components/IdCardConsoleComponent.cs
@@ -47,7 +47,7 @@ public sealed partial class IdCardConsoleComponent : Component
     {
         "Armory",
         //"Atmospherics",
-        "Bailiff", // Frontier
+        "TDFWarden", // Triad
         //"Bar",
         "Brig",
         "Detective",
@@ -63,7 +63,7 @@ public sealed partial class IdCardConsoleComponent : Component
         "External",
         "Frontier", // Frontier
         "HeadOfPersonnel",
-        "HeadOfSecurity",
+        "ChiefEnforcer", // Triad
         //"Hydroponics",
         "Janitor",
         //"Kitchen",
@@ -79,7 +79,7 @@ public sealed partial class IdCardConsoleComponent : Component
         //"ResearchDirector",
         //"Salvage",
         "Security",
-        "Sergeant", // Frontier
+        "SectionLeader", // Triad
         "Service",
         "StationTrafficController", // Frontier
         //"USSP", // Mono
