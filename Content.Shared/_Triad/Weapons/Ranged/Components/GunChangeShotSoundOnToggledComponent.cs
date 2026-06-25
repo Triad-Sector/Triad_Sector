22465fe941a9a20ca81318bec3d5ec688b52272d
TheRealMasterChief117 <officialtrollfacexxx@gmail.com>
2026-06-25 17:39:38 -0400

    a

--- Content.Shared/_Triad/Weapons/Ranged/Components/GunChangeShotSoundOnToggledComponent.cs
+++ Content.Shared/_Triad/Weapons/Ranged/Components/GunChangeShotSoundOnToggledComponent.cs
@@ -7,7 +7,7 @@ namespace Content.Shared._Triad.Weapons.Ranged.Components;
 /// Changes the gunshot noise when it is toggled via <see cref="ItemToggleSystem"/>.
 /// </summary>
 [RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
-public sealed partial class GunChangeShotSoundOnToggled : Component
+public sealed partial class GunChangeShotSoundOnToggledComponent : Component
 {
     [DataField(required: true), AutoNetworkedField]
     public SoundSpecifier? Sound;
