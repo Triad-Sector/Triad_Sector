9dcef383f1e86b9a23ffcbcaac7ed0c98da1a675
TheRealMasterChief117 <officialtrollfacexxx@gmail.com>
2026-06-25 20:38:12 -0400

    Apply suggestion from @TheRealMasterChief117

--- Content.Shared/_Triad/Weapons/Ranged/Components/BlockWieldOnUntoggledComponent.cs
+++ Content.Shared/_Triad/Weapons/Ranged/Components/BlockWieldOnUntoggledComponent.cs
@@ -4,7 +4,7 @@ using Robust.Shared.Toolshed.Commands.GameTiming;
 namespace Content.Shared._Triad.Weapons.Ranged.Components;
 
 /// <summary>
-/// Blocks wielding when BlockWieldOnUntoggled is set to true.
+/// Blocks wielding when the datafield BlockWieldOnUntoggled is set to true.
 /// </summary>
 [RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
 public sealed partial class BlockWieldOnUntoggledComponent : Component
