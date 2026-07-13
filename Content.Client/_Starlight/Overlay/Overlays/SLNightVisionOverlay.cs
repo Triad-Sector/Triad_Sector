cb5438f482db24eb79803a38b62394f615688c33
TheRealMasterChief117 <officialtrollfacexxx@gmail.com>
2026-07-13 14:20:33 -0400

    remove starlight stuff in prepration for phosphor NVG

--- Content.Client/_Starlight/Overlay/Overlays/SLNightVisionOverlay.cs
+++
@@ -1,9 +0,0 @@
-using Robust.Client.Graphics;
-
-namespace Content.Client._Starlight.Overlay;
-
-public sealed class SLNightVisionOverlay : BaseVisionOverlay
-{
-    public SLNightVisionOverlay(ShaderPrototype shader) : base(shader)
-        => ZIndex = (int?)OverlayZIndexes.SLNightVision;
-}
