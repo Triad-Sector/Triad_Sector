57dcb0ef8d482be56cdd1eb21c4c94e9109c70df
asoggycroissant <iinanopandaii@gmail.com>
2026-08-08 18:48:50 -0500

    Capitalized BaseTemperature

--- Content.Shared/Temperature/Components/TemperatureSpeedComponent.cs
+++ Content.Shared/Temperature/Components/TemperatureSpeedComponent.cs
@@ -13,7 +13,7 @@ public sealed partial class TemperatureSpeedComponent : Component
     /// Sets the baseline temperature to compare with thresholds to know when to stop applying a slowdown
     /// </summary>
     [DataField]
-    public float? baseTemperature;
+    public float? BaseTemperature;
 
     /// <summary>
     /// Pairs of temperature thresholds to applied slowdown values.
