82f2ce8890595e1b9c20e08c70b649c9c166528c
mnva <218184747+mnva0@users.noreply.github.com>
2026-09-01 13:20:01 -0400

    Requested changes

--- Content.Server/_Mono/FireControl/FireControlSystem.cs
+++ Content.Server/_Mono/FireControl/FireControlSystem.cs
@@ -5,7 +5,7 @@ using Content.Server.Weapons.Ranged.Systems;
 using Content.Shared._Mono.FireControl;
 using Content.Shared.Power;
 using Content.Shared.Weapons.Ranged.Components;
-using Content.Shared.Weapons.Ranged.Events;
+using Content.Shared.Weapons.Ranged.Events; // Triad
 using Robust.Shared.Map;
 using Robust.Shared.Physics;
 using Robust.Shared.Physics.Systems;
@@ -52,7 +52,6 @@ public sealed partial class FireControlSystem : EntitySystem
         SubscribeLocalEvent<FireControllableComponent, PowerChangedEvent>(OnControllablePowerChanged);
         SubscribeLocalEvent<FireControllableComponent, ComponentShutdown>(OnControllableShutdown);
         SubscribeLocalEvent<FireControllableComponent, EntParentChangedMessage>(OnControllableParentChanged);
-        SubscribeLocalEvent<FireControllableComponent, ShotAttemptedEvent>(OnShotAttempted); // Triad
 
         // Subscribe to grid split events to ensure we update when grids change
         SubscribeLocalEvent<GridSplitEvent>(OnGridSplit);
@@ -98,6 +97,7 @@ public sealed partial class FireControlSystem : EntitySystem
     }
 
     // Triad - Prevent firing guns unless controlled.
+    [SubscribeLocalEvent]
     private void OnShotAttempted(EntityUid uid, FireControllableComponent component, ref ShotAttemptedEvent args)
     {
         if (component.ControllingServer == null)
